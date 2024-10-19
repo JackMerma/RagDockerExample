fb1d2dc160f06da9218961d88dad2d117324f6f8
Jackson Merma <jmermap@unsa.edu.pe>
2024-10-19 17:44:20 -0500

    feat: adding swagger connection

--- src/app.py
+++ src/app.py
@@ -2,9 +2,31 @@ import os
 from flask import Flask, request, jsonify
 from rag.methods import handler_ranking_number, build_retriever, retriever_content
 from langchain_chatbot.controller import build_response
+from flask_swagger_ui import get_swaggerui_blueprint
 
 # Global variables
 app = Flask(__name__)
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://petstore.swagger.io/v2/swagger.json'
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
 
 """
 @app.errorhandler(Exception)
